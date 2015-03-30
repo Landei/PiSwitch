4868db4e805b9e310bd8d81d63cba4081441ce75
Landei <andreas-dresden@gmx.net>
2015-03-30 15:28:06 +0200

    Update status.php

--- status.php
+++ status.php
@@ -2,5 +2,4 @@
 	$s1 = file_get_contents('./status/status_01', FILE_USE_INCLUDE_PATH);
 	$s2 = file_get_contents('./status/status_02', FILE_USE_INCLUDE_PATH);
 	$s3 = file_get_contents('./status/status_03', FILE_USE_INCLUDE_PATH);
-	$s4 = file_get_contents('./status/status_04', FILE_USE_INCLUDE_PATH);
 ?>
