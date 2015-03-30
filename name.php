b741bd3551488e7e0d4b80c96695acac52150059
Landei <andreas-dresden@gmx.net>
2015-03-30 15:27:28 +0200

    Update name.php

--- name.php
+++ name.php
@@ -2,9 +2,8 @@
 
  header("Content-Type: text/html; charset=UTF-8");
 
- $name1 = 'Schreibtischlampe';
- $name2 = 'Lüfter';
- $name3 = 'Luefter 2';
- $name4 = 'Kette 1';
+ $name1 = 'Steckdose 1';
+ $name2 = 'Steckdose 2';
+ $name3 = 'Steckdose 3';
 
 ?>
