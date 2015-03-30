7a3b2e343734a335a0d396e0e5e30a6f34eb772c
Landei <andreas-dresden@gmx.net>
2015-03-30 15:36:59 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,7 @@ header("Content-Type: text/html; charset=UTF-8");
 
 /* Konfiguration */
 
-$code = "01001"; //Code der DIP-Schalter der Steckdosen
+$code = "00000"; //Code der DIP-Schalter der Steckdosen (0 = OFF; 1 = ON)
 $path = "/home/pi/rcswitch-pi";  //Pfad zu rcswitch (Standart: /home/pi/rcswitch-pi; muss evtl. geaendert werden)
+
 ?>
