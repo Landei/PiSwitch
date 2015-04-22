9350c4564c73b2f397ea85d42cfa78f3d7544a45
overflowerror <mail@overflowerror.com>
2015-04-22 22:06:00 +0200

    another header. do you know what headers are?

--- param.php
+++ param.php
@@ -3,16 +3,15 @@
 /* Prueft die evtl. uebergebenen Parameter */
 
 include 'config.php';
-header("Content-Type: text/html; charset=UTF-8");
 
-    foreach($_GET as $key in $value) {
-        if (substr($key, 0, 4) != "sent")
-            continue;
-        $num = substr($key, 3, 1);
-        $state = substr($key, 4, 1);
-        shell_exec($path . '/send ' . $code . ' ' . $num . ' ' . $state);
-        shell_exec('./script/regenerate.sh ' . $num . $state);
-    }
+foreach($_GET as $key in $value) {
+    if (substr($key, 0, 4) != "sent")
+        continue;
+    $num = substr($key, 3, 1);
+    $state = substr($key, 4, 1);
+    shell_exec($path . '/send ' . $code . ' ' . $num . ' ' . $state);
+    shell_exec('./script/regenerate.sh ' . $num . $state);
+}
 
 if(isset($_GET["kette1"])){
  shell_exec('sudo script/kette1.sh'); // ???
